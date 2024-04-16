dc38d0e86f8ea1b8f3fccfb66e8e935d62633046
Karita Kostiainen <kar@umbraco.dk>
2024-04-16 14:30:53 +0200

    added HSTS

--- src/UmbracoProject/Program.cs
+++ src/UmbracoProject/Program.cs
@@ -1,5 +1,15 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+//HSTS options, maxage = 2 years 
+builder.Services.AddHsts(options =>
+{
+	options.Preload = true;
+	options.IncludeSubDomains = true;
+	options.MaxAge = TimeSpan.FromSeconds(63072000);
+	options.ExcludedHosts.Add("example.com");
+	options.ExcludedHosts.Add("www.example.com");
+});
+
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
@@ -10,6 +20,12 @@ builder.CreateUmbracoBuilder()
 WebApplication app = builder.Build();
 
 await app.BootUmbracoAsync();
+
+//security course addition for HSTS, excercise 4
+if (!app.Environment.IsDevelopment())
+{
+	app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
