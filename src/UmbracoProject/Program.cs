b990bc0eef55b0c8b68e495a9c4fa8238ae5a11c
Karita <kar@umbraco.dk>
2024-07-02 11:19:46 +0200

    CSP rules added

--- src/UmbracoProject/Program.cs
+++ src/UmbracoProject/Program.cs
@@ -1,3 +1,6 @@
+using Joonasw.AspNetCore.SecurityHeaders;
+using System.Threading.Tasks;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 //HSTS options, maxage = 2 years 
@@ -28,6 +31,34 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+/** clickjacking prevention
+app.Use(async (context, next) =>
+{
+	context.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+	context.Response.Headers.Add("X-XSS-Protection", "1; mode=block");
+	context.Response.Headers.Add("Content-Security-Policy", "default-src 'self' packages.umbraco.org our.umbraco.org;script-src 'self' ajax.googleapis.com unpkg.com ajax.aspnetcdn.com cdnjs.cloudflare.com cdn.jsdelivr.net;style-src 'self' 'unsafe-inline' fonts.googleapis.com cdn.jsdelivr.net cdnjs.cloudflare.com cdn.linearicons.com;img-src 'self' via.placeholder.com data: *.googleapis.com;font-src 'self' cdnjs.cloudflare.com fonts.gstatic.com cdn.linearicons.com frame-ancestors 'self'"); 
+    await next();
+});
+**/
+
+//CSP
+app.UseCsp(csp =>
+{
+	csp.ByDefaultAllow.FromSelf().From("packages.umbraco.org our.umbraco.org");
+	csp.AllowScripts.FromSelf().From("ajax.googleapis.com unpkg.com ajax.aspnetcdn.com cdnjs.cloudflare.com cdn.jsdelivr.net");
+	csp.AllowStyles.FromSelf().From("fonts.googleapis.com cdn.jsdelivr.net cdnjs.cloudflare.com cdn.linearicons.com").AllowUnsafeInline();
+	csp.AllowImages.FromSelf().From("data: via.placeholder.com");
+	csp.AllowFonts.FromSelf().From("data: cdnjs.cloudflare.com fonts.gstatic.com cdn.linearicons.com");
+
+	csp.AllowFraming.FromSelf();
+
+	csp.OnSendingHeader = context =>
+	{
+		context.ShouldNotSend = context.HttpContext.Request.Path.StartsWithSegments("/umbraco");
+		return Task.CompletedTask;
+	};
+});
 
 app.UseUmbraco()
     .WithMiddleware(u =>
